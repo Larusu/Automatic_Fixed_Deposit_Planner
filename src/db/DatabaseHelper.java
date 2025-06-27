3889fa47744392164a2b8bc1e62cfd02defd42f3
JhervisArevalo <jrareval110104@gmail.com>
2025-06-27 13:06:58 +0800

    add files

--- src/db/DatabaseHelper.java
+++ src/db/DatabaseHelper.java
@@ -1,5 +1,19 @@
 package src.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseHelper {
-    
+    private static final String DB_URL = "jdbc:sqlite:db/planner.db";
+
+    public static Connection connect() {
+        try {
+            return DriverManager.getConnection(DB_URL);
+        } catch (SQLException e) {
+            System.out.println("Database connection failed.");
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
