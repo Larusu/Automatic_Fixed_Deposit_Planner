79c77264d8566a996dc7b7a27b60c32d7589ba5b
Larusu <ldaneliee33@gmail.com>
2025-06-27 15:15:32 +0800

    DatePicker on the hosuzzzz

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+package src;
+
+import java.util.Properties;
+
+import javax.swing.JFrame;
+import org.jdatepicker.impl.*;
+
+import src.DateFomat.DateLabelFormatter;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame("Date Picker Example");
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Configure the date picker
+        UtilDateModel model = new UtilDateModel();
+        Properties p = new Properties();
+        p.put("text.today", "Today");
+        p.put("text.month", "Month");
+        p.put("text.year", "Year");
+        
+        JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
+        JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+
+        frame.getContentPane().add(datePicker);
+        frame.setVisible(true);
+    }
+    
+}
