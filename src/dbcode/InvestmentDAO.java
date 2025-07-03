2ea0b314569f1ba1d628b570526ba3c5b1f35eb7
Larusu <ldaneliee33@gmail.com>
2025-07-03 18:42:52 +0800

    fixing the files again

--- src/dbcode/InvestmentDAO.java
+++ src/dbcode/InvestmentDAO.java
@@ -1,4 +1,4 @@
-package model;
+package dbcode;
 
 public class InvestmentDAO {
     
--- src/model/Investment.java
+++ src/model/Investment.java
@@ -8,10 +8,14 @@ public class Investment {
     private int estimated_tax;
 
     Investment(double deposit_id, double maturity_amount, int total_interest, int estimated_tax){
-        
+
         this.deposit_id = deposit_id;
         this.maturity_amount = maturity_amount;
         this.total_interest = total_interest;
         this.estimated_tax = estimated_tax;
     }
+
+    public double getMaturityAmount(){ return maturity_amount;}
+    public double getMTotalInterest(){ return total_interest;}
+    public double getEstimateTax(){ return estimated_tax;}
 }
