3c1d08cfc1aa3a53441a6bdad009479d956fe7d0
Larusu <ldaneliee33@gmail.com>
2025-07-17 01:34:54 +0800

    added tax

--- src/logic/InterestCalculator.java
+++ src/logic/InterestCalculator.java
@@ -2,10 +2,31 @@ package logic;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 public class InterestCalculator{
 
-    private final int taxRate = 20; // 20% 
+    // For resident and non-resident aliens engaged in trade or business in the Philippines, 
+    // the maximum rate on income subject to final tax (usually passive investment income) is 20%.
+    // "https://taxsummaries.pwc.com/philippines/individual/taxes-on-personal-income#:~:text=For%20resident%20and%20non%2Dresident,rate%20is%20a%20flat%2025%25."
+    private final int taxRate = 20; 
+
+    public static void main(String[] args) {
+
+        InterestCalculator calc = new InterestCalculator();
+        int principal = 1000;
+        double rate = 1;
+        int time = 5;
+        String duration = "monthly";
+        System.out.println("Principal: " + principal);
+        System.out.println("Rate: " + rate + "%");
+        System.out.println("Time: " + time + " years");
+        System.out.println("Duration: " + duration);
+        System.out.println("Tax: " + calc.taxRate + "%");
+        System.out.println("Total Interest paid: " + calc.totalTaxPaid(principal, rate, duration, time));
+        System.out.println("Maturity With Tax: " + calc.compoundInterestWithTax(principal, rate, duration, time));
+        System.out.println("Maturity: " + calc.compoundInterest(principal, rate, duration, time));
+    }
 
     public double compoundInterest(int principal, double rate, String duration, int time)
     {
@@ -42,6 +63,15 @@ public class InterestCalculator{
         
         return c.setScale(2, RoundingMode.HALF_UP).doubleValue(); // get 2 decimal places that is rounded
     } 
+
+    public double totalTaxPaid(int principal, double rate, String duration, int time)
+    {
+        double d = compoundInterest(principal, rate, duration, time) - compoundInterestWithTax(principal, rate, duration, time);
+
+        BigDecimal taxPaid = BigDecimal.valueOf(d);
+
+        return taxPaid.setScale(2, RoundingMode.HALF_UP).doubleValue();
+    }
 
     private int strFrequency(String frequency)
     {
