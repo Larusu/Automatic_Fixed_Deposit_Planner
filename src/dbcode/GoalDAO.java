cced7af6002d0432ed3eead46eaffd8bcea7e1ac
Larusu <ldaneliee33@gmail.com>
2025-07-19 00:54:42 +0800

    added allgoals for goals to get all the available goals

--- src/dbcode/GoalDAO.java
+++ src/dbcode/GoalDAO.java
@@ -1,6 +1,12 @@
 package dbcode;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
 
 import model.Goal;
 
@@ -26,5 +32,34 @@ public class GoalDAO extends CrudDAO<Goal>{
         fields.add(g.getPrice());
 
         return fields;
+    }
+
+    ArrayList<Goal> getAllGoal()
+    {
+        ArrayList<Goal> goal = new ArrayList<>();
+
+        try(Connection conn = DatabaseInitializer.connect();
+        Statement stmt = conn.createStatement();)
+        {
+            String sql = "SELECT id, name FROM goal";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            if(rs.next())
+            {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+
+                goal.add(new Goal(id, name));
+            }
+
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+            System.err.println("Couldn't get goal names.");
+            return null;
+        }
+
+        return goal;
     }
 }
--- src/model/Goal.java
+++ src/model/Goal.java
@@ -1,11 +1,12 @@
 package model;
 
-public class Goal{
-
-   private String name;
-   private int timeframeValue;
-   private String timeframeUnit;
-   private double price;
+public class Goal
+{
+    private int id;
+    private String name;
+    private int timeframeValue;
+    private String timeframeUnit;
+    private double price;
 
     public Goal(String name, int timeframe_value, String timeframe_unit, double price)
     {
@@ -14,9 +15,18 @@ public class Goal{
         this.timeframeUnit = timeframe_unit;
         this.price = price;
     }
+    public Goal(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
 
+    public int getGoalId(){ return id; }
     public String getName(){ return name;}
     public int getTimeframeValue(){ return timeframeValue;}
     public String getTimeframeUnit(){ return timeframeUnit;}
     public double getPrice(){ return price;}
+    
+    @Override
+    public String toString() { return name; }
 }
