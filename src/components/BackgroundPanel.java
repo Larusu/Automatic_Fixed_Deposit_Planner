643fbda29e03d321e31cbfdcb4eead1ca02e8a82
JhervisArevalo <jrarevalo110104@gmail.com>
2025-07-21 13:43:12 +0800

    Create BackgroundPanel.java

---
+++ src/components/BackgroundPanel.java
@@ -0,0 +1,20 @@
+package components;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class BackgroundPanel extends JPanel {
+    private Image backgroundImage;
+
+    public BackgroundPanel() {
+        backgroundImage = new ImageIcon(getClass().getResource("/images/login_bg.png")).getImage();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+    }
+}
