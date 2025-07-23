53406c9efd1ff0af7ac3185a194c47f841d61f0f
Larusu <ldaneliee33@gmail.com>
2025-07-24 02:06:59 +0800

    updated getalldata on depositplan

--- src/main/java/dbcode/DepositPlanDAO.java
+++ src/main/java/dbcode/DepositPlanDAO.java
@@ -2,15 +2,12 @@ package dbcode;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JOptionPane;
-
-import logic.TimeReference.Frequency;
 import model.DepositPlan;
 import session.Session;
 
